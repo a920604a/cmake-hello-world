cbbf97efb1f64ead6e90aa50e9420fe851f9caf8
a920604a <a920604a@gmail.com>
2022-08-14 02:38:06 +0000

    add header file

---
+++ include/Tim.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+
+class Tim
+{
+public:
+    inline void boo()
+    {
+        std::cout << "Tim!\n";
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Tim.h"
+int main()
+{
+    std::cout << "Hello World\n";
+    Tim().boo();
+    return 0;
+}
